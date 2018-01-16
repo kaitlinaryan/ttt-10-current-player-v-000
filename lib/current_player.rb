c6fbba9f17812183d5da12c28eb0e6ee1d7c3df0
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-01-16 16:53:25 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,7 @@
 def turn_count(board)
   players_character = ["X", "O"]
-  count = 0
-if valid_move
+  counter = 1
+  players_character.each do |character|
+  puts "Number of moves is #{counter}"
   counter += 1
 end
